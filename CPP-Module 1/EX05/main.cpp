af20de8559b31befa556c9db9cc6eaa0f52d07f1
ababdelo <ababdelo.imdstl@gmail.com>
2023-11-30 17:43:42 +0100

    Addin' 'CPP-Module\ 1/EX05'

---
+++ CPP-Module 1/EX05/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ababdelo <ababdelo@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 17:20:42 by ababdelo          #+#    #+#             */
+/*   Updated: 2023/11/30 17:20:42 by ababdelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main( void ) {
+
+    std::string input;
+    Harl        harl;
+
+    do
+    {
+        std::cout << "Enter a level: ";
+        std::cin >> input;
+        harl.complain(input);
+    } while (input.compare("EXIT"));
+
+    return 0;
+}
