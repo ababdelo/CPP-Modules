4d358edc723e04bbe717da3d6e488c7a65d4b19e
ababdelo <ababdelo.imdstl@gmail.com>
2023-11-30 17:40:29 +0100

    Addin' 'CPP-Module\ 1/EX02'

---
+++ CPP-Module 1/EX02/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ababdelo <ababdelo@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 16:54:17 by ababdelo          #+#    #+#             */
+/*   Updated: 2023/11/30 16:54:17 by ababdelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main( void )
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+    std::cout << &str << ": " << str << std::endl;
+    std::cout << stringPTR << ": " << *stringPTR << std::endl;
+    std::cout << &stringREF << ": " << stringREF << std::endl;
+
+    return 0;
+}
